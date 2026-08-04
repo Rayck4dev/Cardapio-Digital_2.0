8cedc09782997b67c8d20b040a4104aa42b8c003
Raycka Castro <rayckadevweb@gmail.com>
2026-08-04 13:27:49 -0400

    refactor: fix files

--- src/pages/Picoles.jsx
+++ src/pages/Picoles.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { supabase } from "../config/supabaseClient";
 import MenuFlutuante from "../components/MenuFlutuante";
 import ChuvaAnimada from "../components/ChuvaAnimada";
-import { textosPicoles } from "../types/textospicoles";
 
 export default function Picolés() {
   const [produtos, setProdutos] = useState([]);
@@ -10,6 +9,23 @@ export default function Picolés() {
   useEffect(() => {
     document.title = "Picolés | Delicias no Pote";
   }, []);
+
+  const textosPicoles = {
+    "Pudim de Chocolate Branco com Caramelo Crocante":
+      "Um picolé cremoso sabor pudim tradicional, trufado com calda de caramelo, cobertura com uma camada de chocolate branco com caramelo crocante",
+
+    "Pudim de Chocolate ao Leite":
+      "Um picolé cremoso sabor pudim tradicional, trufado com calda de caramelo, cobertura com uma camada crocante de chocolate ao leite",
+
+    Prestígio:
+      "Um picolé cremoso sabor Prestígio, trufado com creme de brigadeiro, coberto com uma camada crocante de chocolate ao leite",
+
+    "Ninho com Nutella":
+      "Um picolé cremoso sabor ninho, trufado com Nutella, coberto com uma camada crocante de chocolate ao leite.",
+
+    "Paçoca com Doce de Leite":
+      "Um picolé cremoso sabor Paçoca, trufado com  Doce de leite, coberto com uma camada crocante de chocolate ao leite.",
+  };
 
   useEffect(() => {
     const fetchProdutos = async () => {
